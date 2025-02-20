5fd79c278bc220d30b7fab66e28d3b4e3faf22c5
Evan Latham <evan.latham@kellogg.northwestern.edu>
2025-02-20 00:49:55 +0000

    copmleted lab 1 bitcoin

--- app/controllers/tacos_controller.rb
+++ app/controllers/tacos_controller.rb
@@ -1,7 +1,11 @@
 class TacosController < ApplicationController
 
   def index
-    # render :template => "tacos/index"
+    # render :template => "tacos/index" 
+    # NOTE: only need this line if the index defined is looking for a different file than the
+  @fillings = ["Carnitas", "Al Pastor", "Steak", "Fish", "Veggie"]
+  
+  
   end
 
 end
